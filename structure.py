8e53e6198d0910ebb598dd3739275433d98e4e00
nathsmo <nmoralesrojas@ufm.edu>
2024-12-03 17:16:04 +0100

    first commit Initial commit

---
+++ structure.py
@@ -0,0 +1,26 @@
+#Script finder
+
+# Situation: I need to know if a movie has been adapted from a book (book series, article, real case) 
+
+# Task: Find if a movie has been adapted from a book or viceversa. 
+#   Additional idea: See what movie aligns better with a book and viceversa
+
+# Action: 
+#   Areas of interest: Books
+    # Description
+    # Reviews (sites, articles, video description)
+    # Articles
+    # Awards
+
+#   Areas of interest: Movies/Series
+    # Description
+    # Reviews (sites, articles, video description)
+    # Fan-fics
+    # Branch series
+    # Awards
+
+#   Possible solutions:
+    # LLM's
+    # Categorization and then nearest neighbor?
+
+# Result
